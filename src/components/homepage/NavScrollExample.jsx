4a083c4e9b540d18f4ef8ee24bdf2aed26fc976b
Tata Christian <tatachristian12@gmail.com>
2026-05-12 16:16:12 +0200

    updating npm build step and docker file 2

--- src/components/homepage/NavScrollExample.jsx
+++ src/components/homepage/NavScrollExample.jsx
@@ -1,6 +1,4 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -51,10 +49,10 @@ function NavScrollExample() {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          
+
           <Link to="/login" class="text-dark login-button text-decoration-none fw-bold">Log in</Link>
-          <Buttons websiteButtons="Open Business Account"/>
-          
+          <Buttons websiteButtons="Open Business Account" />
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
